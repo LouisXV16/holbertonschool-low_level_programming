8744ec1c8832a34c199ef9890c5cb81bc352770c
LouisXV16 <louis.saintmachin@gmail.com>
2024-02-19 15:21:21 +0100

    write a C program that prints a sentence

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-int main(void)
-
 /**
  * main - Program that print exatly
  *
- * Always : Return 0
+ * Return: Always 0
  */
 
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
