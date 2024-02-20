e0fac5d267b7339f0cb45fd03314a189915e56cd
LouisXV16 <louis.saintmachin@gmail.com>
2024-02-20 15:56:14 +0100

    display the alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lowercase
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	init x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		putchar(x);
+	putchar(10);
+	return (0);
+}
