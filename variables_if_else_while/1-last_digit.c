9434316716646b81cf6164e8ba597031e7120e5f
LouisXV16 <louis.saintmachin@gmail.com>
2024-02-20 12:00:40 +0100

    info about the last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -15,14 +15,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("Last digit of %d is ", n);
-
-	int last_digit(int n);
-
-		return (n % 10);
-	if (n > 5)
+	if (n % 10 > 5)
 		printf("and is greater than 5\n");
 	else
-		if (n == 0)
+		if (n % 10 == 0)
 			printf("and is 0\n");
 		else
 			printf("and is less than 6 and not 0\n");
