2868aa3dbc88738eec5070b59c3a8ff54f3de04b
LouisXV16 <louis.saintmachin@gmail.com>
2024-02-23 21:20:31 +0100

    add 2 integers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - add 2 integers
+ * @num1: number 1
+ * @num2: number 2
+ * Return: result of add
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
