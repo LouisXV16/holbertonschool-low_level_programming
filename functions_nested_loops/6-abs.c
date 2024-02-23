25e479000d00fcc01a69ecc0e5d5435d548a1025
LouisXV16 <louis.saintmachin@gmail.com>
2024-02-23 13:44:03 +0100

    compute the absolute value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - compute the absolute value of an integrer
+ *@n number to be checked
+ * Return: -n if n<0, n else
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
