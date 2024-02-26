eb5b4e2eb5f5ce5d6a5fb6681d74c897ddcb48cd
LouisXV16 <louis.saintmachin@gmail.com>
2024-02-26 13:46:52 +0100

    prints a square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - that prints a square
+ * @size: size of the square
+ * Return: always 0
+ */
+
+void print_square(int size)
+{
+	int x, l;
+
+	if (size > 0)
+	{
+		for (l = 1; l <= size; l++)
+		{
+			for (x = 1; x <= size; x++)
+				_putchar('#');
+		_putchar(10);
+		}
+	}
+}
