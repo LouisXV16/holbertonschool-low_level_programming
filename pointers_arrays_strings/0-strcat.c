ca0aa49c1c28d3f03392347bae8533f32b58e739
LouisXV16 <louis.saintmachin@gmail.com>
2024-03-01 15:12:53 +0100

    main.h

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -13,13 +13,13 @@ char *_strcat(char *dest, char *src)
 	int len1;
 	int len2;
 
-	for (len1 = 0; src[len1] != '\0'; len1++)
+	for (len1 = 0; dest[len1] != '\0'; len1++)
 	{
 	}
 	for (len2 = 0; src[len2] != '\0'; len2++)
 	{
 		dest[len1] = src[len2];
-		len2++;
+		len1++;
 	}
 	dest[len1] = '\0';
 	return (dest);
