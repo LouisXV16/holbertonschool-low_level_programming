55a5dc56bf7984b59f355a2070a704562bf0f3d2
LouisXV16 <louis.saintmachin@gmail.com>
2024-03-03 19:35:40 +0100

    prints a triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - print a triangle
+ * @size:
+ *
+ * Return: Always
+ */
+
+void print_triangle(int size)
+{
+	int l, c;
+
+	if (size > 0)
+	{
+		for (l = 1; l <= size; l++)
+		{
+			for (c = size - ligne; c > 0; c--)
+			{
+				_putchar(' ');
+			}
+			for (c = l; c > 0; c--)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
+
