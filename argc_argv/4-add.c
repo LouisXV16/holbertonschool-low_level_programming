3f793517c6f32e6ca3855d31d171d4d0a6b846c6
LouisXV16 <louis.saintmachin@gmail.com>
2024-03-06 20:04:58 +0100

    v1

---
+++ argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - print prod of number of arg
+*@argc: size of argv
+*@argv: array of program
+*Return: always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int numi;
+
+	if (argc == 1)
+		printf("0\n");
+		return (0);
+
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	numi = atoi(argv[i]);
+
+	printf("%d\n", numi + );
+
+	return (0);
+}
+
