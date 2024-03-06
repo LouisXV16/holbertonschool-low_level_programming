79372fc0d47231556547a3d9d9f5a996970236b6
LouisXV16 <louis.saintmachin@gmail.com>
2024-03-06 17:36:05 +0100

    check if is a prime number

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+*_prime - check if prime
+*@n: number
+*@x: dividende
+*Return: 0 or 1
+*/
+
+int _prime(int n, int x)
+{
+	if (x * x > n)
+		return (1);
+	if (x * x == n)
+		return (0);
+	return (_prime(n, x + 1));
+}
+
+
+/**
+*is_prime_number - return if is a prime number
+*@n: number
+*/
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	if (n % 2 == 0)
+		return (0);
+	else
+		return (_prime(n, 2));
+}
