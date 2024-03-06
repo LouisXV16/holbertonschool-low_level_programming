565f5395f6a1dc3a46d5d7b71ab7f64ad81f1812
LouisXV16 <louis.saintmachin@gmail.com>
2024-03-06 19:50:48 +0100

    multiplie the 2 arg

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,12 +12,17 @@ int main(int argc, char *argv[])
 {
 	int num1, num2;
 
-	(void)argc;
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
 
 	printf("%d\n", num1 * num2);
+
 	return (0);
 }
 
