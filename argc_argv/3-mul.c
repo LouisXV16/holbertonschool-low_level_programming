d95109779df704f1e94fffbb69814335160914fe
LouisXV16 <louis.saintmachin@gmail.com>
2024-03-06 19:42:47 +0100

    multiplies 2 num

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - print prod of number of arg
+*@argc: size of argv
+*@argv: array of program
+*Return: always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+
+	(void)argc;
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	printf("%d\n", num1 * num2);
+	return (0);
+}
+
