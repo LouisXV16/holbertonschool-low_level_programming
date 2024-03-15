a432b020e372055ea0ee801ac3c3d844a36a0e81
LouisXV16 <louis.saintmachin@gmail.com>
2024-03-15 15:33:49 +0100

    create an array of int, v2

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -18,7 +18,7 @@ int *array_range(int min, int max)
 		return (NULL);
 	p = malloc(sizeof(int) * (max - min) + sizeof(int));
 
-	if (p == 0)
+	if (p == NULL)
 		return (NULL);
 	while (min <= max)
 	{
