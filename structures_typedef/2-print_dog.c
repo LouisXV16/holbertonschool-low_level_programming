76e056d3e69cb66bd8aa6f6d8b1addfc65a6336d
LouisXV16 <louis.saintmachin@gmail.com>
2024-03-15 13:43:10 +0100

    print infos of the struct, v3

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -9,11 +9,11 @@
 void print_dog(struct dog *d)
 {
 	if (d->name == NULL)
-		printf("Name: (nil)");
+		printf("Name: (nil)\n");
 	if (d->age < 0)
-		printf("Age: (nil)");
+		printf("Age: (nil)\n");
 	if (d->owner == NULL)
-		printf("Owner: (nil)");
+		printf("Owner: (nil)\n");
 	if (d == NULL)
 	{
 	}
