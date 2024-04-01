557aea0ef69bbafbe30b5ca6d4865ab11de12a4f
LouisXV16 <louis.saintmachin@gmail.com>
2024-04-01 12:56:19 +0200

    add library + move prototype after the structure

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,8 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -18,5 +19,7 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
+size_t print_list(const list_t *h);
 
 #endif
