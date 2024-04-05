9f5fcd41f619ced26e568a6c322a361ae42bb2b5
LouisXV16 <louis.saintmachin@gmail.com>
2024-04-05 16:56:23 +0200

    fix betty style, whitespace

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -6,15 +6,16 @@
 * @m: nb 2 to compare
 * Return: nb of différent bits in n & m
 */
+
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    unsigned long int difference = n ^ m;
-    unsigned int count = 0;
+	unsigned long int difference = n ^ m;
+	unsigned int count = 0;
 
-    while (difference > 0)
-    {
-        count += difference & 1;
-        difference >>= 1;
-    }
-    return (count);
+	while (difference > 0)
+	{
+	count += difference & 1;
+	difference >>= 1;
+	}
+	return (count);
 }
